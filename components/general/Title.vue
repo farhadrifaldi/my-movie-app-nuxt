5ef7d14f2265e06ed3ba22566e22f1f272064a6e
farhad <farhadrifaldi@gmail.com>
2025-02-19 20:33:13 +0700

    update title component props

--- components/general/Title.vue
+++ components/general/Title.vue
@@ -1,22 +1,22 @@
 <template>
-  <div class="relative">
+  <div class="relative font-medium">
     <hr class="border border-[#E74C3C] w-[100px]" />
-    <h1 v-if="level === '1'" class="py-3 font-bold text-6xl">
+    <h1 v-if="level === '1'" class="py-3 text-6xl">
       <slot />
     </h1>
-    <h2 v-if="level === '2'" class="py-3 font-bold text-5xl">
+    <h2 v-if="level === '2'" class="py-3 text-5xl">
       <slot />
     </h2>
-    <h3 v-if="level === '3'" class="py-3 font-bold text-4xl">
+    <h3 v-if="level === '3'" class="py-3 text-4xl">
       <slot />
     </h3>
-    <h4 v-if="level === '4'" class="py-3 font-bold text-3xl">
+    <h4 v-if="level === '4'" class="py-3 text-3xl">
       <slot />
     </h4>
-    <h5 v-if="level === '5'" class="py-3 font-bold text-2xl">
+    <h5 v-if="level === '5'" class="py-3 text-2xl">
       <slot />
     </h5>
-    <h6 v-if="level === '6'" class="py-3 font-bold text-xl">
+    <h6 v-if="level === '6'" class="py-3 text-xl">
       <slot />
     </h6>
   </div>
