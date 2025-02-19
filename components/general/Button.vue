093f0585b45ad372f90cca4ca40e8d7a5d4067c8
farhad <farhadrifaldi@gmail.com>
2025-02-19 19:51:08 +0700

    add button component

---
+++ components/general/Button.vue
@@ -0,0 +1,14 @@
+<template>
+  <RouterLink v-if="to" class="py-1 px-8 bg-red-600 rounded-3xl cursor-pointer font-medium" :to="to">
+    <slot></slot>
+  </RouterLink>
+  <button v-else class="py-1 px-8 bg-red-600 rounded-3xl cursor-pointer font-medium">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts" setup>
+const { to } = defineProps<{ to?: string }>()
+</script>
+
+<style></style>
