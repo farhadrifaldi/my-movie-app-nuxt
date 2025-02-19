4fae8b43d687b813660d23b70df15c85d413349d
farhad <farhadrifaldi@gmail.com>
2025-02-19 20:16:19 +0700

    add component typograph title

---
+++ components/general/Title.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="relative">
+    <hr class="border border-[#E74C3C] w-[100px]" />
+    <h1 v-if="level === '1'" class="py-3 font-bold text-6xl">
+      <slot />
+    </h1>
+    <h2 v-if="level === '2'" class="py-3 font-bold text-5xl">
+      <slot />
+    </h2>
+    <h3 v-if="level === '3'" class="py-3 font-bold text-4xl">
+      <slot />
+    </h3>
+    <h4 v-if="level === '4'" class="py-3 font-bold text-3xl">
+      <slot />
+    </h4>
+    <h5 v-if="level === '5'" class="py-3 font-bold text-2xl">
+      <slot />
+    </h5>
+    <h6 v-if="level === '6'" class="py-3 font-bold text-xl">
+      <slot />
+    </h6>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { level } = defineProps<{
+  level: '1' | '2' | '3' | '4' | '5' | '6'
+}>()
+</script>
