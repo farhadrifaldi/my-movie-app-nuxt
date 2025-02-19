4126553c39c0ff2dca94c956225bc097fbcd92bb
farhad <farhadrifaldi@gmail.com>
2025-02-19 20:33:02 +0700

    update button component props

--- components/general/Button.vue
+++ components/general/Button.vue
@@ -1,17 +1,15 @@
 <template>
   <RouterLink v-if="to"
-    :class="`py-1 px-8 rounded-3xl cursor-pointer font-medium ${!type || type === 'primary' ? 'bg-red-600' : 'bg-[#15181e]'}`"
-    :to="to">
+    :class="`py-1 px-8 rounded-3xl cursor-pointer font-medium ${active ? 'bg-red-600' : 'bg-[#15181e]'}`" :to="to">
     <slot></slot>
   </RouterLink>
-  <button v-else
-    :class="`py-1 px-8 rounded-3xl cursor-pointer font-medium ${!type || type === 'primary' ? 'bg-red-600' : 'bg-[#15181e]'}`">
+  <button v-else :class="`py-1 px-8 rounded-3xl cursor-pointer font-medium ${active ? 'bg-red-600' : 'bg-[#15181e]'}`">
     <slot></slot>
   </button>
 </template>
 
 <script lang="ts" setup>
-const { to, type } = defineProps<{ to?: string, type?: 'primary' | 'secondary' }>()
+const { to, active } = defineProps<{ to?: string, active?: boolean }>()
 </script>
 
 <style></style>
