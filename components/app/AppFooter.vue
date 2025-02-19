f868eeb3b62b40e84469f9211b848aed29573496
farhad <farhadrifaldi@gmail.com>
2025-02-19 12:17:23 +0700

    update footer style

--- components/app/AppFooter.vue
+++ components/app/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full h-28 bg-black/30">
+    <div class="w-full h-28 bg-black/30 z-10">
         <div class="container mx-auto flex justify-between items-center h-full">
             <p class="font-extralight opacity-70">© 2021 MoovieTime. All rights reserved.</p>
             <img src="/images/MoovieTime-Logo.svg" alt="movie-logo" class="grayscale opacity-35">
