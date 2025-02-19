4daf553ab6d33f710470ead7438447e279f1e1c5
farhad <farhadrifaldi@gmail.com>
2025-02-19 12:17:43 +0700

    add ux to show search result

--- components/app/AppHeaderSearch.vue
+++ components/app/AppHeaderSearch.vue
@@ -1,7 +1,21 @@
+<script setup lang="ts">
+import { Icon } from '#components';
+
+const text = ref("")
+</script>
+
 <template>
     <div class="relative w-1/2">
         <span class="absolute left-2 top-2"><img src="/images/movie-icon.svg" /></span>
-        <input type="text" class="bg-[#1e232b] border-0 h-10 rounded-sm w-full px-10" placeholder="Find Movie" />
-        <Icon name="mdi-light:magnify" class="absolute right-2 top-2 text-neutral-500" size="25px" />
+        <input v-model="text" type="text" class="bg-[#1e232b] border-0 h-10 rounded-sm w-full px-10"
+            placeholder="Find Movie" />
+        <Icon v-if="text.length < 1" name="mdi-light:magnify" class="absolute right-2 top-2 text-neutral-500"
+            size="25px" />
+        <ul v-if="text.length > 4" class="absolute w-full bg-black rounded-b-lg z-50 py-4">
+            <li class="hover:bg-white/10 py-1 px-4">Test</li>
+            <li class="hover:bg-white/10 py-1 px-4">Test</li>
+            <li class="hover:bg-white/10 py-1 px-4">Test</li>
+            <li class="hover:bg-white/10 py-1 px-4">Test</li>
+        </ul>
     </div>
 </template>
