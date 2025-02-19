27705665755f61d66c0b65fa5ea44011c0040775
farhad <farhadrifaldi@gmail.com>
2025-02-19 17:29:21 +0700

    responsive footer

--- components/app/AppFooter.vue
+++ components/app/AppFooter.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="w-full h-28 bg-black/30 z-10">
-        <div class="container mx-auto flex justify-between items-center h-full">
+        <div class="container mx-auto flex flex-col-reverse justify-center md:justify-between items-center h-full">
             <p class="font-extralight opacity-70">© 2021 MoovieTime. All rights reserved.</p>
-            <img src="/images/MoovieTime-Logo.svg" alt="movie-logo" class="grayscale opacity-35">
-            <p class="font-extralight opacity-70">Made with NuxtJS</p>
+            <img src="/images/MoovieTime-Logo.svg" alt="movie-logo" class="grayscale opacity-35 mb-4 md:mb-0">
+            <p class="font-extralight opacity-70 hidden md:block">Made with NuxtJS</p>
         </div>
     </div>
 </template>
