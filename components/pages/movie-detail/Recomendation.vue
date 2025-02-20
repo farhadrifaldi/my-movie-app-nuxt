6315463a6e35fa274a57d93726e8be739f79ef57
farhad <farhadrifaldi@gmail.com>
2025-02-20 10:20:39 +0700

    add recommendation for movie detail page

---
+++ components/pages/movie-detail/Recomendation.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container mx-auto py-16">
+    <p class="font-medium text-lg mb-10">RECOMMENDATION MOVIES</p>
+    <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
+      <MovieCard v-for="_ in 5" rating="1.0" title="Below Zero" year="2021" genre="Action" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
