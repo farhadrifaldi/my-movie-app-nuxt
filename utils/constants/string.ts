181b9c711146ab0b7029c4d9b3c4280b24ccf466
farhad <farhadrifaldi@gmail.com>
2025-02-20 18:06:19 +0700

    add utilitites to convert number to money format

--- utils/constants/string.ts
+++ utils/constants/string.ts
@@ -1,1 +1,5 @@
 export const TMDB_IMAGE_BASE_PATH = 'https://image.tmdb.org/t/p/w500'
+
+export function money(num: number) {
+    return num.toLocaleString("en-US", { style: "currency", currency: "USD" });
+}
