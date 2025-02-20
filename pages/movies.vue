9521a6933758ad8bef35607657fe3ca166bbfcfe
farhad <farhadrifaldi@gmail.com>
2025-02-20 08:07:56 +0700

    create new component page

---
+++ pages/movies.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="relative">
+    <div class="absolute top-0 left-0 w-full h-[450px] bg-[#292e36] -z-20">&nbsp;</div>
+    <div class="container mx-auto py-8">
+      <GeneralTitle level="3">Movies</GeneralTitle>
+      <div class="grid grid-cols-12 gap-10 mt-8">
+        <div class="col-span-3">
+          <PagesMoviesFilter />
+        </div>
+        <div class="col-span-9">
+          <PagesMoviesCards @load-more="onLoadMore" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+function onLoadMore() {
+  console.log('load')
+}
+</script>
+
+<style></style>
