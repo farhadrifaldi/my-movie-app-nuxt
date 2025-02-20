01746ca465ac3d2ceccdfeac8c6dabc51f53a2d9
farhad <farhadrifaldi@gmail.com>
2025-02-20 14:38:34 +0700

    update button component to emit event

--- components/general/Button.vue
+++ components/general/Button.vue
@@ -3,13 +3,18 @@
     :class="`py-1 px-8 rounded-3xl cursor-pointer font-medium ${active ? 'bg-red-600' : 'bg-[#15181e]'}`" :to="to">
     <slot></slot>
   </RouterLink>
-  <button v-else :class="`py-1 px-8 rounded-3xl cursor-pointer font-medium ${active ? 'bg-red-600' : 'bg-[#15181e]'}`">
+  <button v-else :class="`py-1 px-8 rounded-3xl cursor-pointer font-medium ${active ? 'bg-red-600' : 'bg-[#15181e]'}`"
+    @click="emit('click')">
     <slot></slot>
   </button>
 </template>
 
 <script lang="ts" setup>
 const { to, active } = defineProps<{ to?: string, active?: boolean }>()
+
+const emit = defineEmits<{
+  (e: 'click'): void
+}>();
 </script>
 
 <style></style>
