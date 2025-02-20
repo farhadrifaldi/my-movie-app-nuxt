a663ae96b369dc8b64d92019f7d68bee32563452
farhad <farhadrifaldi@gmail.com>
2025-02-20 08:07:39 +0700

    add components card

---
+++ components/pages/movies/Cards.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="grid grid-cols-2 lg:grid-cols-4 md:grid-cols-3 gap-6">
+    <MovieCard v-for="movie in 10" rating="1.0" title="Below Zero" year="2021" genre="Action" />
+  </div>
+  <div class="flex justify-center mt-10">
+    <GeneralButton active @click="onLoadMore">Load More</GeneralButton>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const emit = defineEmits<{
+  (e: 'loadMore'): void
+}>();
+
+function onLoadMore() {
+  emit('loadMore')
+}
+
+</script>
+
+<style></style>
