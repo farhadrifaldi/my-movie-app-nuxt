95b61081492c1820df78ec692f00ab818982a3ac
farhad <farhadrifaldi@gmail.com>
2025-02-20 18:05:32 +0700

    update genres on movie catalogue

--- components/pages/movies/Cards.vue
+++ components/pages/movies/Cards.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="grid grid-cols-2 xl:grid-cols-4 md:grid-cols-3 gap-6">
     <MovieCard v-for="movie in movies" :rating="movie.vote_average.toString()" :title="movie.title"
-      :year="$dayjs(movie.release_date).format('YYYY')" genre="Action"
-      :image-path="TMDB_IMAGE_BASE_PATH + movie.poster_path" />
+      :year="$dayjs(movie.release_date).format('YYYY')" :image-path="TMDB_IMAGE_BASE_PATH + movie.poster_path"
+      :to="'/movies/' + movie.id" :genre="genreStore.getMappedGenres(movie.genre_ids, 1)" />
   </div>
   <div class="flex justify-center mt-10">
     <GeneralButton active @click="onLoadMore">Load More</GeneralButton>
@@ -13,6 +13,7 @@
 import { TMDB_IMAGE_BASE_PATH } from '~/utils/constants/string'
 import type { Movie } from '~/utils/types/movies';
 
+const genreStore = useGenreStore()
 const { movies } = defineProps<{ movies: Movie[] }>()
 
 const emit = defineEmits<{
