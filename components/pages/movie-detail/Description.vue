31748d4688e0a80500d0afaa0ea3ce4b893d5343
farhad <farhadrifaldi@gmail.com>
2025-02-20 10:20:22 +0700

    add review on movie detail

---
+++ components/pages/movie-detail/Description.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="bg-white w-full py-10">
+    <div class="container text-black mx-auto grid grid-cols-12">
+      <div class="col-span-3 hidden lg:block"></div>
+      <div class="col-span-12 lg:col-span-5">
+        <p class="font-semibold text-red-600 mb-2">OVERVIEW</p>
+        <p>Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s and finds a
+          formidable foe by the name of the Cheetah.</p>
+      </div>
+    </div>
+    <div class="container mx-auto my-5">
+      <p class="font-semibold text-red-600">REVIEWS</p>
+      <div class="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-12 mt-8">
+        <MovieReview v-for="_ in 2" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
