efa75375974213f75068100f9302ae85f5a6548a
farhad <farhadrifaldi@gmail.com>
2025-02-20 10:20:15 +0700

    add banner section for movie detail

---
+++ components/pages/movie-detail/Banner.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="flex items-end bg-[url(/images/movie-image-banner-sample.jpg)] backdrop-brightness-75 bg-cover h-[500px]">
+    <div class="backdrop-brightness-50 w-full h-full flex items-end flex-wrap">
+      <div class="w-full">
+        <div class="container mx-auto w-full grid grid-cols-12 items-center text-sm mb-5">
+          <div class="col-span-3 hidden lg:block"></div>
+          <div class="col-span-12 md:col-span-8 text-center md:text-left">
+            <p class="text-lg mb-2">2020</p>
+            <p class="text-5xl font-semibold mb-2">Wonder Woman 1984</p>
+            <p class="">Fantasy, Action, Adventure</p>
+          </div>
+        </div>
+        <div class="w-full bg-black/65 h-auto lg:h-[80px] py-5">
+          <div class="mx-auto container items-center grid grid-cols-12 text-sm h-full gap-3">
+            <div class="col-span-3 hidden lg:block"></div>
+            <div class="col-span-12 lg:col-span-9 flex flex-wrap justify-center lg:justify-start gap-6">
+              <div class="flex w-full lg:w-auto justify-center">
+                <div class="flex mr-3">
+                  <Icon name="material-symbols:star" size="40" class="text-yellow-400 mr-2" />
+                  <span class="font-semibold text-4xl">7.0</span>
+                </div>
+                <div>
+                  <p class="font-extralight text-neutral-400">USER SCORE</p>
+                  <p>3621 VOTES</p>
+                </div>
+              </div>
+              <div class="text-center md:text-left">
+                <p class="font-extralight text-neutral-400">STATUS</p>
+                <p>RELEASED</p>
+              </div>
+              <div class="text-center md:text-left">
+                <p class="font-extralight text-neutral-400">LANGUAGE</p>
+                <p>ENGLISH</p>
+              </div>
+              <div class="text-center md:text-left">
+                <p class="font-extralight text-neutral-400">BUDGET</p>
+                <p>$200,000,000.00</p>
+              </div>
+              <div class="text-center md:text-left">
+                <p class="font-extralight text-neutral-400">PRODUCTION</p>
+                <p>DC ENTERTAINMENT</p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container mx-auto hidden lg:block relative">
+    <div class="absolute col-span-2 -bottom-28 left-0">
+      <img src="/images/movie-sample-image.jpg" alt="image" class="h-[330px] w-[220px] object-cover">
+    </div>
+  </div>
+</template>
