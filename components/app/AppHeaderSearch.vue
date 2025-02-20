b27832c54364edd97925ce0b38da012202309a13
farhad <farhadrifaldi@gmail.com>
2025-02-20 18:03:36 +0700

    wiring search

--- components/app/AppHeaderSearch.vue
+++ components/app/AppHeaderSearch.vue
@@ -1,7 +1,29 @@
 <script setup lang="ts">
 import { Icon } from '#components';
+import { useQuery } from '@tanstack/vue-query';
+import axios, { type AxiosResponse } from 'axios';
+import type { TmdbResponse } from '~/utils/types/movies';
 
 const text = ref("")
+
+const fetcher = async (search: string): Promise<AxiosResponse<TmdbResponse, any>> => await axios.get('/api/movies/search', {
+    params: {
+        search: search
+    }
+})
+
+const { data, isLoading } = useQuery({
+    queryKey: ['discover-movies', text], queryFn: async ({ queryKey }): Promise<TmdbResponse> => {
+        if (queryKey[1].length > 4) {
+            const { data } = await fetcher(queryKey[1].toString())
+            return data
+        }
+        return {
+            results: []
+        }
+    }
+})
+
 </script>
 
 <template>
@@ -14,10 +36,9 @@ const text = ref("")
                 size="25px" />
         </span>
         <ul v-if="text.length > 4" class="absolute w-full bg-black rounded-b-lg z-50 py-4">
-            <li class="hover:bg-white/10 py-1 px-4">Test</li>
-            <li class="hover:bg-white/10 py-1 px-4">Test</li>
-            <li class="hover:bg-white/10 py-1 px-4">Test</li>
-            <li class="hover:bg-white/10 py-1 px-4">Test</li>
+            <li v-for="movie in data.results" class="hover:bg-white/10 py-1 px-4 text-white w-full">
+                <RouterLink :to="'/movies/' + movie.id" @click="text = ''">{{ movie.title }}</RouterLink>
+            </li>
         </ul>
     </div>
 </template>
