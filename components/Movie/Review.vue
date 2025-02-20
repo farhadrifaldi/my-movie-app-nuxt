62c45923ce3c26122fde47cf6d5cb3446c767015
farhad <farhadrifaldi@gmail.com>
2025-02-20 18:04:34 +0700

    update review component

--- components/Movie/Review.vue
+++ components/Movie/Review.vue
@@ -4,25 +4,27 @@
       <div class="flex items-center">
         <div class="bg-neutral-300 w-[50px] h-[50px] rounded-full mr-3">&nbsp;</div>
         <div>
-          <p class="font-semibold">SWITCH.</p>
-          <p class="text-sm text-neutral-600">December 18, 2020</p>
+          <p class="font-semibold">{{ review.author }}</p>
+          <p class="text-sm text-neutral-600">{{ $dayjs(review.created_at).format('MMMM DD, YYYY') }}</p>
         </div>
       </div>
       <div class="flex items-start bg-neutral-200 rounded-md px-2 py-3">
         <Icon name="material-symbols:star" size="24" class="text-yellow-400 mr-1" />
-        <span class="font-semibold text-4xl">7.0</span>
+        <span class="font-semibold text-4xl">{{ review.author_details.rating }}</span>
       </div>
     </div>
-    <p class="mt-5 italic">It isn't as easy as saying 'Wonder Woman 1984' is a good or bad movie. The pieces are there, and there are
-      moments I adore, but it does come across as a bit of a mess, even though the action sequences are breathtaking.
-      If you're a fan of the original film, you'll be more willing to take the ride, but for those more indifferent,
-      it may be a bit of a blander sit. If you can and are planning to watch it, the theatrical experience is the way
-      to go - there is nothing like seeing these stunning sets, fun action scenes and hearing Zimmer's jaw-dropping
-      score like on the big screen. - Chris dos Santos... read the rest.</p>
+    <p class="mt-5 italic">{{ trimDesc(review.content) }}</p>
   </div>
 </template>
 
 <script lang="ts" setup>
+import type { MovieReview } from '~/utils/types/movies';
+
+const { review } = defineProps<{ review: MovieReview }>()
+
+function trimDesc(desc: string) {
+  return desc.length > 100 ? desc.substring(0, 500) + '...' : desc;
+}
 
 </script>
 
