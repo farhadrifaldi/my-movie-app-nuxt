346907ae6831a59ead7ca752cde6dd0f05457e21
farhad <farhadrifaldi@gmail.com>
2025-02-20 10:20:48 +0700

    create review component movie

---
+++ components/Movie/Review.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="bg-neutral-100 text-black p-4 shadow-md rounded-lg">
+    <div class="flex justify-between">
+      <div class="flex items-center">
+        <div class="bg-neutral-300 w-[50px] h-[50px] rounded-full mr-3">&nbsp;</div>
+        <div>
+          <p class="font-semibold">SWITCH.</p>
+          <p class="text-sm text-neutral-600">December 18, 2020</p>
+        </div>
+      </div>
+      <div class="flex items-start bg-neutral-200 rounded-md px-2 py-3">
+        <Icon name="material-symbols:star" size="24" class="text-yellow-400 mr-1" />
+        <span class="font-semibold text-4xl">7.0</span>
+      </div>
+    </div>
+    <p class="mt-5 italic">It isn't as easy as saying 'Wonder Woman 1984' is a good or bad movie. The pieces are there, and there are
+      moments I adore, but it does come across as a bit of a mess, even though the action sequences are breathtaking.
+      If you're a fan of the original film, you'll be more willing to take the ride, but for those more indifferent,
+      it may be a bit of a blander sit. If you can and are planning to watch it, the theatrical experience is the way
+      to go - there is nothing like seeing these stunning sets, fun action scenes and hearing Zimmer's jaw-dropping
+      score like on the big screen. - Chris dos Santos... read the rest.</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
