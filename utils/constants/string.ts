d5a632acade39706d06c443aae87af78563a8613
farhad <farhadrifaldi@gmail.com>
2025-02-21 09:36:43 +0700

    update size image base path

--- utils/constants/string.ts
+++ utils/constants/string.ts
@@ -1,4 +1,4 @@
-export const TMDB_IMAGE_BASE_PATH = (type?: 'poster' | 'backdrop') => `https://image.tmdb.org/t/p/${type === 'poster' ? 'w500' : 'w1280'}`
+export const TMDB_IMAGE_BASE_PATH = (type?: 'poster' | 'backdrop') => `https://image.tmdb.org/t/p/${type === 'backdrop' ? 'w1280' : 'w500'}`
 
 export function money(num: number) {
     return num.toLocaleString("en-US", { style: "currency", currency: "USD" });
