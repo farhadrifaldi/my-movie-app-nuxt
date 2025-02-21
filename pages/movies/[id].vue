a9361463b33719db0b951d6f5d2b47a08fff382b
farhad <farhadrifaldi@gmail.com>
2025-02-21 09:41:42 +0700

    serverside rendering movie detail

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -5,20 +5,12 @@
 </template>
 
 <script lang="ts" setup>
-import axios, { type AxiosResponse } from 'axios';
 import type { Movie } from '~/utils/types/movies';
 
-const movie = ref<Movie | null>(null)
 const route = useRoute()
 const { id } = route.params
-const fetcher = async (id: string): Promise<AxiosResponse<Movie, any>> => await axios.get('/api/movies/' + id)
 
-await useAsyncData(`movie-${id}`, async () => {
-  fetcher(id as string).then((response) => {
-    movie.value = response.data
-  })
-})
-
+const { data: movie } = await useFetch<Movie>('/api/movies/' + id)
 
 </script>
 
