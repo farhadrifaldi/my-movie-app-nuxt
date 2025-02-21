9f7f2e9ffdee342c79509e24ee8c9f4b17d24041
farhad <farhadrifaldi@gmail.com>
2025-02-21 09:04:01 +0700

    implement db content on movie catalog

--- components/pages/movies/Cards.vue
+++ components/pages/movies/Cards.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="grid grid-cols-2 xl:grid-cols-4 md:grid-cols-3 gap-6">
-    <MovieCard v-for="movie in movies" :rating="movie.vote_average.toString()" :title="movie.title"
-      :year="$dayjs(movie.release_date).format('YYYY')" :image-path="TMDB_IMAGE_BASE_PATH + movie.poster_path"
-      :to="'/movies/' + movie.id" :genre="genreStore.getMappedGenres(movie.genre_ids, 1)" />
-  </div>
+  <GeneralDbContent :is-loading="isLoading" :is-error="isError">
+    <div class="grid grid-cols-2 xl:grid-cols-4 md:grid-cols-3 gap-6">
+      <MovieCard v-for="movie in movies" :rating="movie.vote_average.toString()" :title="movie.title"
+        :year="$dayjs(movie.release_date).format('YYYY')" :image-path="TMDB_IMAGE_BASE_PATH + movie.poster_path"
+        :to="'/movies/' + movie.id" :genre="genreStore.getMappedGenres(movie.genre_ids, 1)" />
+    </div>
+  </GeneralDbContent>
   <div class="flex justify-center mt-10">
     <GeneralButton active @click="onLoadMore">Load More</GeneralButton>
   </div>
@@ -14,7 +16,7 @@ import { TMDB_IMAGE_BASE_PATH } from '~/utils/constants/string'
 import type { Movie } from '~/utils/types/movies';
 
 const genreStore = useGenreStore()
-const { movies } = defineProps<{ movies: Movie[] }>()
+const { movies, isError, isLoading } = defineProps<{ movies: Movie[], isLoading: boolean, isError: boolean }>()
 
 const emit = defineEmits<{
   (e: 'loadMore'): void
