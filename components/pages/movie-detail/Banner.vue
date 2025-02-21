a05560fc949b2d1520a802578fb1e60e78c6f56b
farhad <farhadrifaldi@gmail.com>
2025-02-21 09:41:25 +0700

    update image backdrop

--- components/pages/movie-detail/Banner.vue
+++ components/pages/movie-detail/Banner.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="`flex items-end bg-center backdrop-brightness-75 bg-cover h-[550px] -mt-16`"
-    :style="`background-image: url(${TMDB_IMAGE_BASE_PATH() + movie.backdrop_path})`">
+    :style="`background-image: url(${TMDB_IMAGE_BASE_PATH('backdrop') + movie.backdrop_path})`">
     <div class="backdrop-brightness-50 w-full h-full flex items-end flex-wrap">
       <div class="w-full">
         <div class="container mx-auto w-full grid grid-cols-12 items-center text-sm mb-5">
